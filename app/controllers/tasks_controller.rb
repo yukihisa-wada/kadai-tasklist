6555f83b405cdd2646efaef1516c029aa6c25591
yukihisa.wada <ywino440@gmail.com>
2021-01-22 06:40:33 +0000

    kadi-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,11 +46,11 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
+  
+  private
 
-private
-
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+end
+
